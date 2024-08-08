1b204103c3853941a8a1668d7bad91ee662f1c61
ilago <ilago@DESKTOP-IK7N945>
2024-08-09 02:00:56 +0300

    New classes

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-a=5
-print(a)
+from tkinter import *
+
+class PyPhotoEditor:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Photo Editor")
+        #self.root.iconbitmap("icons/icon.img")
+
+        self.root.bind("Escape", self._close)
+   
+    def run(self):
+        self.drawMenu()
+        self.drawWigets()
+
+        self.root.mainloop()
+
+    def _close(self, event):
+        self.root.quit()
+    
+    def drawMenu(self):
+        pass
+
+    def drawWigets(self):
+        pass
+
+if __name__ == "__main__":
+    PyPhotoEditor().run()
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\ilago\Documents\Polaroid-Cut\venv
+set VIRTUAL_ENV=D:\Users\ilago\Documents\PhotoCut\polaroid-kiwi\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
